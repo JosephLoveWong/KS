1797044491f868b9fe852f35b94bc28e347c1d2b
joseph <JosephLoveWong@gmail.com>
2017-08-23 00:08:47 +0800

    spiderView step 1

--- app/src/main/java/com/promiseland/ks/view/custom/DemoView.java
+++ app/src/main/java/com/promiseland/ks/view/custom/DemoView.java
@@ -16,12 +16,14 @@ import android.view.View;
 public class DemoView extends View {
     private Paint mPaint = new Paint();
 
-    private int count = 6;
-    private int capacity = 5;
-    private float mRadius;
+    private int mDimension = 5;
+    private int mCapacity = 5;
 
-    private float mWidth;
-    private float mHeight;
+    private float mRadius;
+    private float mAngle;
+
+    private int centerX;
+    private int centerY;
 
     public DemoView(Context context) {
         this(context, null);
@@ -43,19 +45,62 @@ public class DemoView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        mWidth = w;
-        mHeight = h;
-        mRadius = Math.min(mWidth, mHeight) / 2;
+
+        mRadius = Math.min(w, h) / 2;
+        centerX = w / 2;
+        centerY = h / 2;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.translate(mWidth / 2, mHeight / 2);
+        mAngle = (float) (Math.PI * 2 / mDimension);
 
+        canvas.rotate(-90, centerX, centerY);
+        drawPolygon(canvas);
+        drawLine(canvas);
+    }
+
+    /**
+     * 绘制正多边形
+     */
+    private void drawPolygon(Canvas canvas) {
         Path path = new Path();
-        path.lineTo(mRadius, 0);
+        path.reset();
+
+        float r = mRadius / mCapacity;
+        for (int i = 0; i < mCapacity; i++) {
+            float radius = (i + 1) * r;
+            for (int j = 0; j < mDimension; j++) {
+                if (j == 0) {
+                    path.moveTo(centerX + radius, centerY);
+                } else {
+                    float x = (float) (centerX + radius * Math.cos(mAngle * j));
+                    float y = (float) (centerY + radius * Math.sin(mAngle * j));
+                    path.lineTo(x, y);
+                }
+            }
+            path.close();
+        }
+
+        canvas.drawPath(path, mPaint);
+    }
+
+    /**
+     * 绘制连线
+     */
+    private void drawLine(Canvas canvas) {
+        Path path = new Path();
+        path.reset();
+
+        float radius = mRadius;
+        for (int j = 0; j < mDimension; j++) {
+            float x = (float) (centerX + radius * Math.cos(mAngle * j));
+            float y = (float) (centerY + radius * Math.sin(mAngle * j));
+            path.moveTo(centerX , centerY);
+            path.lineTo(x, y);
+        }
 
         canvas.drawPath(path, mPaint);
     }
